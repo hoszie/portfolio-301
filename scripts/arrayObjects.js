ef1417111a28230e1337a2890a503e05d215a935
nhoszko@gmail.com <nhoszko@gmail.com>
2017-11-19 14:03:53 -0800

    Stretch goals included, truncated template

--- scripts/arrayObjects.js
+++ scripts/arrayObjects.js
@@ -2,19 +2,20 @@
 
 //////////////////////// Array of music objects ///////////////////////
 
-var musicPiecesObjArr = [
+const musicPiecesObjArr = [
     {
         title: 'Humoreske, op. 20',
         composer: 'Robert Schumann',
-        description: '<p>Humoreske in B-flat major, Op. 20, is a romantic piano piece by Robert Schumann, composed in 1839 and dedicated to Julie von Webenau. Schumann cited Jean Paul\'s style of humour as source of inspiration, although there are no direct programmatic links to Jean Paul\'s oeuvre found in the piece.<br>The Humoreske consists of seven sections (not originally indicated as such by the composer except for the last one, "Zum Beschluss"), to be played attacca after each other. Although the piece is nominally written in B-flat major, most of the piece is set in B-flat major\'s relative minor key, G minor. The musical texture and emotional tone, though, varies widely and differs greatly between the sections<p>',
+        description: '<p class="inlinePara">Humoreske in B-flat major, Op. 20, is a romantic piano piece by Robert Schumann, composed the piece in 1839 and dedicated to Julie von Webenau. Schumann cited Jean Paul\'s style of humour as source of inspiration, although there are no direct programmatic links to Jean Paul\'s oeuvre found in the piece.<br><br>The Humoreske consists of seven sections (not originally indicated as such by the composer except for the last one, "Zum Beschluss"), to be played attacca after each other. Although the piece is nominally written in B-flat major, most of the piece is set in B-flat major\'s relative minor key, G minor. The musical texture and emotional tone, though, varies widely and differs greatly between the sections<p>',
         pieceUrl: 'https://soundcloud.com/user-398697111/sets/humoreske-op20',
         image: 'images/schumann.jpg',
         recorded: '2008-12-02'
     },
+
     {
         title: 'Quintet for Piano & Winds in E<sup>b</sup> major, K.452',
         composer: 'W.A. Mozart',
-        description: '<p>The Quintet in E flat major for Piano and Winds, K. 452, was completed by Wolfgang Amadeus Mozart on March 30, 1784 and premiered two days later at the Imperial and Royal National Court Theater in Vienna. Shortly after the premiere, Mozart wrote to his father that "I myself consider it to be the best thing I have written in my life." It is scored for piano, oboe, clarinet, horn and bassoon.</p>',
+        description: '<p class="inlinePara">The Quintet in E flat major for Piano and Winds, K. 452, was completed by Wolfgang Amadeus Mozart on March 30, 1784 and premiered two days later at the Imperial and Royal National Court Theater in Vienna. Shortly after the premiere, Mozart wrote to his father that "I myself consider it to be the best thing I have written in my life." It is scored for piano, oboe, clarinet, horn and bassoon.</p>',
         pieceUrl: 'https://soundcloud.com/user-398697111/sets/quintet-for-piano-winds-in-eb-major-k452',
         image: 'images/mozart.jpeg',
         recorded: '2006-06-24'
@@ -23,11 +24,21 @@ var musicPiecesObjArr = [
     {
         title: 'The Pleasure Dome of Kubali Khan',
         composer: 'Charles Thomlinson Griffes',
-        description: '<p>The hypnotic tone poem "The Pleasure Dome of Kubla Khan" came to American composer Charles Tomlinson Griffes in a dream. And it maintains that dreamy, evocative quality.</p>',
+        description: '<p class="inlinePara">The hypnotic tone poem "The Pleasure Dome of Kubla Khan" came to American composer Charles Tomlinson Griffes in a dream. And it maintains that dreamy, evocative quality.</p>',
         pieceUrl: 'https://soundcloud.com/user-398697111/the-pleasure-dome-of-kublai-khan',
         image: 'images/griffes.jpg',
         recorded: '2007-11-06'
     }
-
-
 ];
+
+
+////////////////////// ARRAY OF ANDY'S WORK ////////////////////////    
+
+// const photographObjArr = [
+//     {
+//         title: 'Every object has a story to tell',
+//         description: 'random single shit',
+//         photoUrl: 'http://worldwideangle.net/studio/fine-art/every-object/',
+//         photoImg: ''
+//     }
+// ];
